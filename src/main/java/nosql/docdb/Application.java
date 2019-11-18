251a13d100227808b874aba4495bde66fa9b3ab7
dimotim <datdimotim@gmail.com>
2019-11-18 22:39:03 +0300

    real data

--- src/main/java/nosql/docdb/Application.java
+++ src/main/java/nosql/docdb/Application.java
@@ -1,18 +1,10 @@
 package nosql.docdb;
 
-import lombok.Value;
 import nosql.docdb.web_application.ServletServer;
 
 
 public class Application {
     public static void main(String[] args) {
         ServletServer.startServer();
-    }
-
-    @Value
-    public static class SimpleDocument{
-        String author;
-        String title;
-        long length;
     }
 }
--- src/main/java/nosql/docdb/web_application/FileInfoWindow.java
+++ src/main/java/nosql/docdb/web_application/FileInfoWindow.java
@@ -3,22 +3,26 @@ package nosql.docdb.web_application;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
+import nosql.docdb.doc_parser.object_model.DbDocument;
+import nosql.docdb.doc_parser.object_model.Picture;
+import nosql.docdb.doc_parser.object_model.Table;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 public  class FileInfoWindow extends Window {
-    public FileInfoWindow(MainView.FileFullInfo record) {
-        super("Дополнительная информация о документе \""+record.getName()+"\""); // Set window caption
+    public FileInfoWindow(DbDocument record) {
+        super("Информация о документе \""+record.getName()+"\"");
         center();
+        setWidth("40%");
         setClosable(true);
         setModal(false);
         VerticalLayout layout = new VerticalLayout();
         setContent(layout);
-        Label dateOfcreate = new Label("Дата создания: "+record.getOfDateCreateString());
-        Label dateOfLoad = new Label("Дата загрузки: "+record.getDateOfLoadString());
-        Label imageCount = new Label("Количество картинок: "+record.getCountOfImages());
-        Label tableCount = new Label("Количество таблиц: "+record.getCountOfTables());
-        layout.addComponents(dateOfcreate,dateOfLoad,imageCount,tableCount);
-        //layout.setExpandRatio(dateOfcreate, 1);
-
+        Label dateOfLoad = new Label("Дата загрузки: "+ LocalDateTime.ofInstant(record.getAddDate(), ZoneId.systemDefault()));
+        Label imageCount = new Label("Количество картинок: "+record.getDocumentObjects().stream().filter(obj->obj instanceof Picture).count());
+        Label tableCount = new Label("Количество таблиц: "+record.getDocumentObjects().stream().filter(obj->obj instanceof Table).count());
+        layout.addComponents(dateOfLoad,imageCount,tableCount);
     }
 }
 
