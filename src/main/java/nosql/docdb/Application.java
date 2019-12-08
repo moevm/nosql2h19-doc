1345cff3a28a3cce99e81e21f1081021b612e1c2
dimotim <datdimotim@gmail.com>
2019-12-08 22:22:30 +0300

    docker version done

--- src/main/java/nosql/docdb/Application.java
+++ src/main/java/nosql/docdb/Application.java
@@ -1,10 +1,17 @@
 package nosql.docdb;
 
+import lombok.SneakyThrows;
+import nosql.docdb.database.MongoDB;
 import nosql.docdb.web_application.ServletServer;
 
 
 public class Application {
+    @SneakyThrows
     public static void main(String[] args) {
-        ServletServer.startServer();
+        if(args.length==2&&args[0].equals("import")){
+            new MongoDB().importFromZip(args[1]);
+        }else {
+            ServletServer.startServer();
+        }
     }
 }
